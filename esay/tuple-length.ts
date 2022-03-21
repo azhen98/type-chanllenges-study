2b32dca6613581341a0a8b41b326e9cad403e0fe
程崇臻 <15939054361@163.com>
2022-03-21 21:14:34 +0800

    添加相关文档

--- esay/tuple-length.ts
+++ esay/tuple-length.ts
@@ -11,3 +11,5 @@ type teslaLength = Length<tesla> // expected 4
 type spaceXLength = Length<spaceX> // expected 5
 
 type Length<T extends readonly any[]> = T['length']
+
+// 相关链接 https://juejin.cn/post/7000560464786620423
