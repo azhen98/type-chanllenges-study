8cbad44150344bc5d36750a4b3fa6ddfc8e0c161
程崇臻 <15939054361@163.com>
2022-04-11 19:56:10 +0800

    realize lookup and promiseAll

--- medium/promiseAll.ts
+++ medium/promiseAll.ts
@@ -1,6 +1,8 @@
+// 正确方法
 declare function PromiseAll<T extends any[]>(
   value: readonly [...T]
 ): Promise<{ [P in keyof T]: T[P] extends Promise<infer E> ? E : T[P] }>
+
 const promise1 = Promise.resolve(3)
 const promise2 = 42
 const promise3 = new Promise<string>((resolve, reject) => {
@@ -8,8 +10,9 @@ const promise3 = new Promise<string>((resolve, reject) => {
 })
 
 // expected to be `Promise<[number, 42, string]>`
-const p = PromiseAll1([promise1, promise2, promise3] as const)
+const p = PromiseAll([promise1, promise2, promise3] as const)
 
+//  错误方法
 declare function PromiseAll1<T extends any[]>(
   values: readonly [...T]
 ): Promise<{ [V in T[number]]: V extends Promise<infer E> ? E : V }>
@@ -21,3 +24,5 @@ type c<T extends any[]> = {
 }
 
 type d = c<a>
+
+// 收获 https://segmentfault.com/q/1010000041682757?_ea=225185614
